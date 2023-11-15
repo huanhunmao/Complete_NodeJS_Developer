18018be24e88c02cef57536ce4ea53b3e9591a99
Marxu <fuhaojun@52tt.com>
2023-11-15 20:28:07 +0800

    feat: 发送图片文件

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,5 +1,10 @@
+const path = require('path');
+
 function getMessages(req, res) {
-    res.send('<ul><li>Hello PPX</li></ul>')
+    // res.send('<ul><li>Hello PPX</li></ul>')
+    // __dirname 表示当前文件夹 
+    // 上一级的 public 下面的 xxx.jpg 图片
+    res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'))
 }
 
 function postMessages(req, res)  {
