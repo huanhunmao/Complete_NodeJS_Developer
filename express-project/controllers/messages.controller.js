9e24eccac1ec5713806fbb95e559f6d179c44b47
Marxu <fuhaojun@52tt.com>
2023-11-15 21:16:37 +0800

    feat: 分离多个 hbs 模版

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -4,7 +4,11 @@ function getMessages(req, res) {
     // res.send('<ul><li>Hello PPX</li></ul>')
     // __dirname 表示当前文件夹 
     // 上一级的 public 下面的 xxx.jpg 图片
-    res.sendFile(path.join(__dirname, '..','images', 'public', 'skimountain.jpg'))
+    // res.sendFile(path.join(__dirname, '..','images', 'public', 'skimountain.jpg'))
+    res.render('messages', {
+        title: 'Hello PPX',
+        friend: 'Marxu'
+    })
 }
 
 function postMessages(req, res)  {
