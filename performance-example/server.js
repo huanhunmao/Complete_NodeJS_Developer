cb1216fcf14728a554bb3a2077b79b755f327997
Marxu <fuhaojun@52tt.com>
2023-11-22 17:05:22 +0800

    feat: 初次使用 集群 cluster

--- performance-example/server.js
+++ performance-example/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cluster = require('cluster')
 
 const app = express()
 
@@ -13,14 +14,25 @@ function delay(duration) {
 
 
 app.get('/', (req, res) => {
-    res.send('Performance example')
+    res.send(`Performance example  ${process.pid}`)
 })
 
 app.get('/timer', (req, res) => {
     delay(9000)
-    res.send('Dinging Dinging Dinging')
+    res.send(`Dinging Dinging Dinging ${process.pid}`)
 })
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-})
+console.log('running server.js');
+// 两个 tab 访问 http://localhost:3000/timer  结果都是 9s 不会变成两倍
+// 一个 tab 访问 /  一个访问 /timer  第一个 非常快
+if(cluster.isMaster) {
+    console.log('Master is been started ... ');
+    cluster.fork()
+    cluster.fork()
+}else{
+    console.log('Worker is been started ... ');
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    })
+}
+
