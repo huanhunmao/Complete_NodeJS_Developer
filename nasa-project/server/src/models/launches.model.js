ad33cdf56d5d025851a4fd3421cef8de6b82a628
Marxu <fuhaojun@52tt.com>
2023-12-04 15:39:24 +0800

    feat: 增加 axios 获取 spaceX API 的数据

--- nasa-project/server/src/models/launches.model.js
+++ nasa-project/server/src/models/launches.model.js
@@ -1,17 +1,18 @@
+const axios = require('axios');
 const launchesDatabase = require('./launches.mongo')
 const planets = require('./planets.mongo')
 
 const DEFAULT_FLIGHT_NUMBER = 100
 
 const launch = {
-    flightNumber: 100,
-    mission: 'Kepler Exploration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    target: 'Kepler-442 b',
-    customers:['ZTM','NASA'],
-    upcoming:true,
-    success: true
+    flightNumber: 100, // flight_number
+    mission: 'Kepler Exploration X', // name
+    rocket: 'Explorer IS1', // rocket.name
+    launchDate: new Date('December 27, 2030'), // date_local
+    target: 'Kepler-442 b', // not applicable
+    customers:['ZTM','NASA'],  // payloads.customers for each payload
+    upcoming:true, // upcoming
+    success: true // success
 }
 
 saveLaunch(launch)
@@ -84,9 +85,57 @@ async function abortLaunchById(launchId) {
     // return aborted
 }
 
+const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query'
+
+async function loadLaunchData(){
+    const response = await axios.post(SPACEX_API_URL,{
+        query: {},
+        options:{
+            // 不分页 拿到所有数据
+            pagination:false,
+            populate:[
+                {
+                    path: 'rocket',
+                    select:{
+                        name:1
+                    }
+                },
+                {
+                    path: 'payloads',
+                    select:{
+                        customers:1
+                    }
+                }
+            ]
+        }
+    })
+
+    const launchDocs = response.data.docs 
+    for(const launchDoc of launchDocs){
+        const payloads = launchDoc.payloads 
+        // 使用 flatMap 将嵌套数组扁平化
+        const customers = payloads.flatMap(payload => payload.customers)
+
+        const launch = {
+            flightNumber: launchDoc.flight_number,
+            mission:launchDoc.name,
+            rocket: launchDoc.rocket.name,
+            launchDate: launchDoc.date_local,
+            customers,
+            upcoming: launchDoc.upcoming,
+            success: launchDoc.success
+        }
+
+        console.log('launch',launch);
+    }
+
+
+}
+
 module.exports = {
     getAllLaunches,
     scheduleNewLaunch,
     existsLaunchWithId,
-    abortLaunchById
+    abortLaunchById,
+    loadLaunchData,
 }
