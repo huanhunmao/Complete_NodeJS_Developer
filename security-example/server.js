a673afdeb4ff6e6a8dc6095281345afb22f6f121
Marxu <fuhaojun@52tt.com>
2023-12-26 18:41:50 +0800

    feat: 使用google 账号登陆

--- security-example/server.js
+++ security-example/server.js
@@ -1,34 +1,130 @@
-const express = require('express');
-const https = require('https');
-const path = require('path');
 const fs = require('fs');
-const helmet = require('helmet')
+const path = require('path');
+const https = require('https');
+const express = require('express');
+const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session');
+const { verify } = require('crypto');
+
+require('dotenv').config();
 
 const PORT = 3000;
 
-const app = express();
-
-app.use(helmet())
-
-app.get('/secret', function (req, res) {
-    return res.send('Your personal secret value is 666');
-});
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-const options = {
-    key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
-    cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
-    // 如果证书有密码，添加 passphrase 选项
-    // passphrase: 'your_certificate_password',
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
-https.createServer(options, app).listen(PORT, function (err) {
+const AUTH_OPTIONS = {
+  callbackURL: '/auth/google/callback',
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log('Google profile', profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+// Save the session to the cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Read the session from the cookie
+passport.deserializeUser((id, done) => {
+  // User.findById(id).then(user => {
+  //   done(null, user);
+  // });
+  done(null, id);
+});
+
+const app = express();
+
+app.use(helmet());
+
+app.use(cookieSession({
+  name: 'session',
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+}));
+app.use((req, res, next) => {
+  // Stub out missing regenerate and save functions.
+  // These don't make sense for client side sessions.
+  if (req.session && !req.session.regenerate) {
+    req.session.regenerate = (cb) => {
+      cb();
+    };
+  }
+  if (req.session && !req.session.save) {
+    req.session.save = (cb) => {
+      cb();
+    };
+  }
+  next();
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+function checkLoggedIn(req, res, next) {
+  console.log('Current user is:', req.user);
+  const isLoggedIn = req.isAuthenticated() && req.user;
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: 'You must log in!',
+    });
+  }
+  next();
+}
+
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['email'],
+  }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: true,
+  }),
+  (req, res) => {
+    console.log('Google called us back!');
+  }
+);
+
+app.get('/auth/logout', (req, res, next) => {
+  //Removes req.user and clears any logged in session
+  req.logout((err) => {
     if (err) {
-        console.error('Error starting server:', err);
-    } else {
-        console.log('Server listening on port ' + PORT);
+      return next(err);
     }
+    res.redirect('/');
+  });
+});
+
+app.get('/secret', checkLoggedIn, (req, res) => {
+  return res.send('Your personal secret value is 42!');
+});
+
+app.get('/failure', (req, res) => {
+  return res.send('Failed to log in!');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+https.createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
